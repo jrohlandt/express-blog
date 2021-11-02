3541cf49cacab8788ad022cc0e73803410face20
Jeandre Rohlandt <jrohlandt@gmail.com>
2021-11-02 10:40:34 +0200

    ts config + basic app and controller

---
+++ src/app.ts
@@ -0,0 +1,45 @@
+import * as express from "express";
+import * as bodyParser from "body-parser";
+
+class App {
+  public app: express.Application;
+  public port: number;
+
+  constructor(controllers, port) {
+    this.app = express();
+    this.port = port;
+
+    this.initializeMiddlewares();
+    this.initializeControllers(controllers);
+  }
+
+  private initializeMiddlewares() {
+    this.app.use(bodyParser.json());
+    this.app.use(this.loggerMiddleware);
+  }
+
+  public loggerMiddleware(
+    request: express.Request,
+    response: express.Response,
+    next
+  ) {
+    console.log(
+      `${request.method} ${request.path}, ${JSON.stringify(request.body)}`
+    );
+    next();
+  }
+
+  private initializeControllers(controllers) {
+    controllers.forEach((controller) => {
+      this.app.use("/", controller.router);
+    });
+  }
+
+  public listen() {
+    this.app.listen(this.port, () => {
+      console.log(`App is listening on port ${this.port}`);
+    });
+  }
+}
+
+export default App;
